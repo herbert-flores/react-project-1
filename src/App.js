8226e9eaa436450de6074b4dc2171111850d8534
herbert flores <herbertmff@hotmail.com>
2021-06-06 16:52:25 +1000

    end props children

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
     return (
-        <div>
+        <Layout>
             {/* //localhost:3000 
             //my-page.com/ */}
-            <MainNavigation />
+
             <Switch>
                 <Route path="/" exact>
                     <AllMeetupsPage />
@@ -21,7 +21,7 @@ function App() {
                     <FavoritesPage />
                 </Route>
             </Switch>
-        </div>
+        </Layout>
     );
 }
 
