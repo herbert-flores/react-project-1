307f4e5e235031d4b2716c99c3605600770be0ae
herbert flores <herbertmff@hotmail.com>
2021-06-06 15:15:34 +1000

    adding Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import { Switch } from "react-router";
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            {/* //localhost:3000 
+            //my-page.com/ */}
+            <Switch>
+                <Route path="/" exact={true}>
+                    <AllMeetupsPage></AllMeetupsPage>
+                </Route>
+                <Route path="/new-meetup">
+                    <NewMeetupPage></NewMeetupPage>
+                </Route>
+                <Route path="/favorites">
+                    <Favorites></Favorites>
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,5 @@
+function NewMeetupPage() {
+    return <div>NewMeetupsPage</div>;
+}
+
+export default NewMeetupPage;
