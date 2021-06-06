1b35f9d69b821206825e714ed17422c6eafdec72
herbert flores <herbertmff@hotmail.com>
2021-06-06 16:40:18 +1000

    props children

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -21,6 +21,7 @@ const DUMMY_DATA = [
 function AllMeetupsPage() {
     return (
         <section>
+            <h1>All Meetups</h1>
             <MeetupList meetups={DUMMY_DATA} />
         </section>
     );
